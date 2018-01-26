a863744631ee3ad40ccd3223048a3b29af59b9c8
Radovan Bast <bast@users.noreply.github.com>
2018-01-26 19:08:25 +0100

    add example data

---
+++ example/example.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cx --boundary="$PWD/boundary.txt" \
+   --islands="$PWD/islands.txt" \
+   --view-angle=20.0 \
+   --min-distance=3.0 \
+   --max-distance=40.0 \
+   --output-dir="$PWD/output"
